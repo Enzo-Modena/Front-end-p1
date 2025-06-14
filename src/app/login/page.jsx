c46a71552defaceebc28cde977c003d7e478ea9c
Dan1ell1 <daniellisz.silva@gmail.com>
2025-06-14 00:49:00 -0300

    Login responsivo

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,31 +1,39 @@
-import './page.module.css'
+import styles from "./page.module.css";
 
 export default function Login() {
-    return(
-        //alteração nos arquivos de page e adicionado a pesagem da fonte poppins
+    return (
+        // alteração nos arquivos de page e adicionado a pesagem da fonte poppins
         <div className="container">
-            {/* Criar conta */}
-            <div className="container-criar-conta">
-                <div className="container-texto">
-                    <h1>Primeira vez aqui?</h1>
-                    <p className="texto">Crie sua conta em poucos minutos e </p>
-                    <p className="texto">tenha acesso a todos os nossos serviços</p>
+            <div className={styles.container}>
+                    <div className={styles.containerGeral}>
+                        {/* Criar conta */}
+                        <div className={styles.containerCriarConta}>
+                            <div className={styles.containerTexto}>
+                                <h1>Primeira vez aqui?</h1>
+                                <p className={styles.texto}>Crie sua conta em poucos minutos e</p>
+                                <p className={styles.texto}>tenha acesso a todos os nossos serviços</p>
+                            </div>
+                            <div className={styles.containerBtnCriarConta}>
+                                <button>Cadastre-se</button>
+                            </div>
+                        </div>
+
+                    {/* Acessar conta */}
+                    <div className={styles.containerAcessarConta}>
+                        <div className={styles.containerTitulo}>
+                            <h1>Acessar conta</h1>
+                        </div>
+                        <form className={styles.forms}>
+                            <div className={styles.campoPreencher}>
+                                <input type="text" placeholder="Email" name="email" />
+                                <input type="text" placeholder="Senha" name="senha" />
+                            </div>
+                            <div className={styles.containerBtnEntrar}>
+                                <button>Login</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-            {/* Acessar conta */}
-            <div className="container-acessar-conta">
-                <div className="container-titulo">
-                    <h1>Acessar conta</h1>
-                </div>
-                <form>
-                    <div className="campo-preencher">
-                    <input type="text" placeholder="Email" name="email"></input>
-                    <input type="text" placeholder="Senha" name="senha"></input>
-                </div>
-                <div className="container-botao">
-                    <button>Login</button>
-                </div>
-                </form>
             </div>
         </div>
     );
